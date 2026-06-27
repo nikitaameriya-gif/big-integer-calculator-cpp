8ea4eb214977de5da27eeb5a194b37168cb3eb98
nikitaameriya-gif <nikitaameriya@gmail.com>
2026-06-27 16:32:32 +0530

    Update BigInteger.h

--- BigInteger.h
+++ BigInteger.h
@@ -9,21 +9,36 @@ using namespace std;
 
 class BigInteger {
 private:
-    vector<int> digits;
+    vector<int> digits;   // stores digits in reverse order
     bool negative;
 
+    void trim();
+    static int compareAbs(const BigInteger &a, const BigInteger &b);
+    static BigInteger addAbs(const BigInteger &a, const BigInteger &b);
+    static BigInteger subAbs(const BigInteger &a, const BigInteger &b);
+
 public:
+    // Constructors
     BigInteger();
     BigInteger(string num);
+    BigInteger(long long num);
 
-    BigInteger operator+(const BigInteger& other);
-    BigInteger operator-(const BigInteger& other);
-    BigInteger operator*(const BigInteger& other);
-    BigInteger operator/(const BigInteger& other);
-    BigInteger operator%(const BigInteger& other);
-    BigInteger operator^(int power);
+    // Utility
+    string toString() const;
 
-    void display() const;
+    // Operators
+    BigInteger operator+(const BigInteger &other) const;
+    BigInteger operator-(const BigInteger &other) const;
+    BigInteger operator*(const BigInteger &other) const;
+    BigInteger operator/(const BigInteger &other) const;
+    BigInteger operator%(const BigInteger &other) const;
+    BigInteger operator^(long long power) const;
+
+    // Comparison
+    bool operator<(const BigInteger &other) const;
+    bool operator==(const BigInteger &other) const;
+
+    friend ostream& operator<<(ostream &out, const BigInteger &num);
 };
 
 #endif
